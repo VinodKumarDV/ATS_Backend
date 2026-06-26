e8dbdc432d78651403535e3f18b082a15703dd7e
VinodKumarDV <86297658+VinodKumarDV@users.noreply.github.com>
2026-06-26 14:07:57 +0530

    Fix CORS allowed origin URL format
    Removed trailing slash from allowed origin URL.

--- app/main.py
+++ app/main.py
@@ -23,7 +23,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://localhost:5173",
-        "https://ats-frontend-bay.vercel.app/"
+        "https://ats-frontend-bay.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
